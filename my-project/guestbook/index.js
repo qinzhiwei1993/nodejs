f6d51aa70331b0ca22c505a6cb822c0f13270eb0
qinzhiwei1993 <384579070@qq.com>
2017-12-04 23:10:48 +0800

    guestbook

---
+++ my-project/guestbook/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var db = require('./models/db.js');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+//静态
+app.use(express.static('./public'));
+
+//设置模板引擎
+app.set('view engine', 'ejs');
+
+//显示留言列表 //增加留言
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+//读数据
+app.get("/du", function(req, res){
+    db.find("guestbook", {}, {pageamount:99, page:1}, function(err, result){
+        if(err){
+            res.json({status: 400, msg: err});
+            return;
+        }
+        res.json({status: 200, data:result});
+    })
+})
+
+//处理留言
+
+app.post('/tijiao', function(req, res){
+    var body = req.body;
+    db.insertOne("guestbook", body, function(err, result){
+        if(err){
+            res.json({status: 400});//给ajax看
+            return;
+        }
+        res.json({status: 200, data: result.ops[0]});
+    })
+    //写入数据库
+})
+
+
+app.listen(3000);
