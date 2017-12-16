65df5fdfd16ad3b54c364e97b596da150d15e311
qinzhiwei1993 <384579070@qq.com>
2017-12-16 15:28:12 +0800

    redis

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ mongodb D:\Git仓库\nodejs\mymongodbdata
     cookie-parser: 解析Cookie标题，并使用由Cookie名称键入的对象填充req.cookies。 或者，您可以通过传递一个秘密字符串来启用签名cookie支持，该字符串指定req.secret，以便其他中间件可以使用该字符串。
     express-session: 设置session
     gm: nodejs 图像处理(剪裁、缩放。。。。。)
+    mongoose: 将javascript对象和数据产生关系的框架。
+    socket.io: 创建websocket协议
+    redis: redis数据库
+    async: 处理nodejs中异步操作
+    under:
+    moment: 格式化时间
---
+++ redis/redis.js
@@ -0,0 +1,21 @@
+var redis = require("redis"),
+    //连接到redis默认库
+    client = redis.createClient();
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
